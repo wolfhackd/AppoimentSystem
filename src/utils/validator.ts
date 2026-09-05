2770d3e725d43978200ea2474231d5ef8417e64a
Mauro Leal <wolfhackd@gmail.com>
2026-09-05 10:17:14 -0300

    feat: validator function

---
+++ src/utils/validator.ts
@@ -0,0 +1,27 @@
+export class Validator {
+    static required(value: unknown, field: string) {
+        if (value === undefined || value === null || value === "") {
+            throw new Error(`${field} é obrigatório`);
+        }
+    }
+
+    static isNumber(value: unknown, field: string) {
+        if (typeof value !== "number") {
+            throw new Error(`${field} deve ser um número`);
+        }
+    }
+
+    static isString(value: unknown, field: string) {
+        if (typeof value !== "string") {
+            throw new Error(`${field} deve ser uma string`);
+        }
+    }
+
+    static isEmail(value: unknown, field: string) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(value as string)) {
+            throw new Error(`${field} deve ser um email válido`);
+        }
+    }
+
+}
